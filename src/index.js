6bb2f226058ebc56f01a592ce5a1e6ebe56ddb92
Alick Shabanovich <alikshabanovich@gmail.com>
2021-10-25 02:09:13 +0300

    feat: add background image and slider features

--- src/index.js
+++ src/index.js
@@ -35,10 +35,10 @@ function showGreeting() {
 function getTimeOfDay() {
   const date = new Date();
   const hours = date.getHours();
-  if (hours > 6 && hours < 11) return "morning";
-  if (hours > 12 && hours < 18) return "day";
-  if (hours > 18 && hours < 24) return "evening";
-  if (hours > 0 && hours < 6) return "night";
+  if (hours >= 6 && hours < 12) return "morning";
+  if (hours >= 12 && hours < 18) return "day";
+  if (hours >= 18 && hours < 24) return "evening";
+  if (hours >= 0 && hours < 6) return "night";
 }
 
 function setLocalStorage() {
@@ -54,3 +54,42 @@ function getLocalStorage() {
   }
 }
 window.addEventListener("load", getLocalStorage);
+
+let randomNum;
+
+function getRandomNum() {
+  randomNum = String(Math.floor(Math.random() * 19 + 1));
+}
+getRandomNum();
+
+function setBg() {
+  const img = new Image();
+  const body = document.querySelector("body");
+  const timeOfDay = getTimeOfDay();
+  if (randomNum < 10) {
+    randomNum = `0${randomNum}`;
+    // randomNum.padStart(2, "0")
+  }
+  console.log(randomNum);
+  const bgNum = randomNum;
+  img.src = `https://raw.githubusercontent.com/shalick/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg`;
+  console.log(img.src);
+  img.onload = () => {
+    body.style.backgroundImage = `url('https://raw.githubusercontent.com/shalick/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg')`;
+  };
+}
+setBg();
+
+function getSlideNext() {
+  randomNum < 20 ? randomNum++ : (randomNum = 1);
+  setBg();
+}
+const slideNext = document.querySelector(".slide-next");
+slideNext.addEventListener("click", getSlideNext);
+
+function getSlidePrev() {
+  randomNum > 1 ? randomNum-- : (randomNum = 20);
+  setBg();
+}
+const slidePrev = document.querySelector(".slide-prev");
+slidePrev.addEventListener("click", getSlidePrev);
