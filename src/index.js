742dc91e597862c2b3a7e9f98523968bbb801945
Alick Shabanovich <alikshabanovich@gmail.com>
2021-10-24 20:53:44 +0300

    feat: add time and date functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-import "./css/style.css"
+// import "./css/style.css";
+
+// import { calculate } from "./js/calc.js";
+
+function showTime() {
+  const date = new Date();
+  const time = document.querySelector(".time");
+  const currentTime = date.toLocaleTimeString();
+  time.textContent = currentTime;
+  showDate();
+  setTimeout(showTime, 1000);
+}
+showTime();
+
+function showDate() {
+  const date = new Date();
+  const dateSelector = document.querySelector(".date");
+  const options = {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+  };
+  const currentDate = date.toLocaleDateString("en-Br", options);
+  dateSelector.textContent = currentDate;
+}
---
+++ src/js/calc.js
@@ -0,0 +1,1 @@
+export const calculate = (a, b) => a + b;
